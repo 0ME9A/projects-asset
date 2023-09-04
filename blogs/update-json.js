8d3aabd19ad19f07c2ea9fc0b00a5a37090f99d8
Baliram Singh <heyome9a@gmail.com>
2023-09-04 17:10:33 +0530

    Update update-json.js
    Signed-off-by: Baliram Singh <heyome9a@gmail.com>

--- blogs/update-json.js
+++ blogs/update-json.js
@@ -1,39 +1,51 @@
 const fs = require('fs');
 const path = require('path');
 
-const docsDirectory = path.join(__dirname, 'blogs/docs');
+const docsDirectory = path.join(__dirname, 'docs');
 const jsonFilePath = path.join(__dirname, 'docs-map.json');
 
 if (!fs.existsSync(docsDirectory)) {
   fs.mkdirSync(docsDirectory, { recursive: true });
 }
 
-const mdxFiles = fs.readdirSync(docsDirectory);
+const subDirectories = fs.readdirSync(docsDirectory).filter((subDir) => {
+  return fs.statSync(path.join(docsDirectory, subDir)).isDirectory();
+});
 
-if (mdxFiles.length > 0) {
+if (subDirectories.length > 0) {
   const existingDocs = fs.existsSync(jsonFilePath) ? require(jsonFilePath) : [];
   const updatedDocs = [];
 
-  mdxFiles.forEach((fileName) => {
-    if (fileName.endsWith('.mdx')) {
-      const docName = fileName.replace('.mdx', '');
-      const existingDoc = existingDocs.find((doc) => doc.main === docName);
+  subDirectories.forEach((subDir) => {
+    const subDirPath = path.join(docsDirectory, subDir);
+    const mdxFiles = fs.readdirSync(subDirPath);
 
-      if (existingDoc) {
-        // Update existing documentation only if the file has changed
-        const fileStats = fs.statSync(path.join(docsDirectory, fileName));
-        if (fileStats.mtime > new Date(existingDoc.updated)) {
-          existingDoc.updated = fileStats.mtime.toISOString();
+    mdxFiles.forEach((fileName) => {
+      if (fileName.endsWith('.mdx')) {
+        const docName = fileName.replace('.mdx', '');
+        const existingDoc = existingDocs.find((doc) => doc.main === subDir);
+
+        if (existingDoc) {
+          // Update existing documentation only if the file has changed
+          const fileStats = fs.statSync(path.join(subDirPath, fileName));
+          if (fileStats.mtime > new Date(existingDoc.updated)) {
+            existingDoc.updated = fileStats.mtime.toISOString();
+          }
+          if (!existingDoc.sub.includes(docName)) {
+            existingDoc.sub.push(docName);
+          }
+          updatedDocs.push(existingDoc);
+        } else {
+          // Create new documentation entry
+          updatedDocs.push({
+            main: subDir,
+            sub: [docName],
+            date: new Date().toISOString(),
+            updated: new Date().toISOString(),
+          });
         }
-        updatedDocs.push(existingDoc);
-      } else {
-        // Create new documentation entry
-        updatedDocs.push({
-          main: docName,
-          updated: new Date().toISOString(),
-        });
       }
-    }
+    });
   });
 
   // Write the updated JSON back to the file
