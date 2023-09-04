5aa298285d63cefb2730835e95241c1b2bbe8fde
Baliram Singh <heyome9a@gmail.com>
2023-09-04 16:54:17 +0530

    Update update-json.js
    Signed-off-by: Baliram Singh <heyome9a@gmail.com>

--- blogs/update-json.js
+++ blogs/update-json.js
@@ -1,33 +1,43 @@
 const fs = require('fs');
 const path = require('path');
 
+const docsDirectory = path.join(__dirname, 'blogs/docs');
 const jsonFilePath = path.join(__dirname, 'docs-map.json');
-const mdxFiles = fs.readdirSync(path.join(__dirname, 'blogs/docs'));
 
-const existingDocs = fs.existsSync(jsonFilePath) ? require(jsonFilePath) : [];
-const updatedDocs = [];
+if (!fs.existsSync(docsDirectory)) {
+  fs.mkdirSync(docsDirectory, { recursive: true });
+}
 
-mdxFiles.forEach((fileName) => {
-  if (fileName.endsWith('.mdx')) {
-    const docName = fileName.replace('.mdx', '');
-    const existingDoc = existingDocs.find((doc) => doc.main === docName);
+const mdxFiles = fs.readdirSync(docsDirectory);
 
-    if (existingDoc) {
-      // Update existing documentation only if the file has changed
-      const fileStats = fs.statSync(path.join(__dirname, 'blogs/docs', fileName));
-      if (fileStats.mtime > new Date(existingDoc.updated)) {
-        existingDoc.updated = fileStats.mtime.toISOString();
+if (mdxFiles.length > 0) {
+  const existingDocs = fs.existsSync(jsonFilePath) ? require(jsonFilePath) : [];
+  const updatedDocs = [];
+
+  mdxFiles.forEach((fileName) => {
+    if (fileName.endsWith('.mdx')) {
+      const docName = fileName.replace('.mdx', '');
+      const existingDoc = existingDocs.find((doc) => doc.main === docName);
+
+      if (existingDoc) {
+        // Update existing documentation only if the file has changed
+        const fileStats = fs.statSync(path.join(docsDirectory, fileName));
+        if (fileStats.mtime > new Date(existingDoc.updated)) {
+          existingDoc.updated = fileStats.mtime.toISOString();
+        }
+        updatedDocs.push(existingDoc);
+      } else {
+        // Create new documentation entry
+        updatedDocs.push({
+          main: docName,
+          updated: new Date().toISOString(),
+        });
       }
-      updatedDocs.push(existingDoc);
-    } else {
-      // Create new documentation entry
-      updatedDocs.push({
-        main: docName,
-        updated: new Date().toISOString(),
-      });
     }
-  }
-});
+  });
 
-// Write the updated JSON back to the file
-fs.writeFileSync(jsonFilePath, JSON.stringify(updatedDocs, null, 2));
+  // Write the updated JSON back to the file
+  fs.writeFileSync(jsonFilePath, JSON.stringify(updatedDocs, null, 2));
+} else {
+  console.log('No MDX files found in the "docs" directory.');
+}
