1c1646ff595fbdc710384684b9301b4e9de523cd
Baliram Singh <heyome9a@gmail.com>
2023-09-06 08:50:36 +0530

    Update and rename update-json.js to update_docs_map.py
    Signed-off-by: Baliram Singh <heyome9a@gmail.com>

---
+++ blogs/update_docs_map.py
@@ -0,0 +1,34 @@
+import os
+import json
+from datetime import datetime
+
+def update_docs_map(docs_dir, docs_map_file):
+    docs_map = []
+    for root, dirs, files in os.walk(docs_dir):
+        if not files:
+            continue
+        main = os.path.relpath(root, docs_dir)
+        sub = []
+        for file in files:
+            if not file.endswith('.mdx'):
+                continue
+            name = os.path.splitext(file)[0]
+            path = os.path.join(root, file)
+            date = os.path.getctime(path)
+            updated_at = os.path.getmtime(path)
+            sub.append({
+                'name': name,
+                'date': datetime.fromtimestamp(date).strftime('%Y-%m-%d %H:%M:%S'),
+                'updated-at': datetime.fromtimestamp(updated_at).strftime('%Y-%m-%d %H:%M:%S')
+            })
+        if not sub:
+            continue
+        docs_map.append({
+            'main': main,
+            'sub': sub
+        })
+    with open(docs_map_file, 'w') as f:
+        json.dump(docs_map, f, indent=4)
+
+
+update_docs_map('blogs/docs', 'blogs/docs-map.json')
