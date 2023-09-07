64bb56949f73037fa88d4def10b8aa2841128983
Baliram Singh <heyome9a@gmail.com>
2023-09-07 11:26:15 +0530

    Update update_docs_map.py
    Signed-off-by: Baliram Singh <heyome9a@gmail.com>

--- blogs/update_docs_map.py
+++ blogs/update_docs_map.py
@@ -15,12 +15,14 @@ def update_docs_map(docs_dir, docs_map_file):
                 continue
             name = os.path.splitext(file)[0]
             path = os.path.join(root, file)
-            date = subprocess.check_output(['git', 'log', '--diff-filter=A', '--format=%aI', '-1', '--', path]).decode().strip()
-            updated_at = subprocess.check_output(['git', 'log', '--diff-filter=M', '--format=%aI', '-1', '--', path]).decode().strip()
+            date_str = subprocess.check_output(['git', 'log', '--diff-filter=A', '--format=%aI', '-1', '--', path]).decode().strip()
+            updated_at_str = subprocess.check_output(['git', 'log', '--diff-filter=M', '--format=%aI', '-1', '--', path]).decode().strip()
+            date = datetime.fromisoformat(date_str).strftime('%Y-%m-%d %H:%M:%S') if date_str else 'N/A'
+            updated_at = datetime.fromisoformat(updated_at_str).strftime('%Y-%m-%d %H:%M:%S') if updated_at_str else 'N/A'
             sub.append({
                 'name': name,
-                'date': datetime.fromisoformat(date).strftime('%Y-%m-%d %H:%M:%S'),
-                'updated-at': datetime.fromisoformat(updated_at).strftime('%Y-%m-%d %H:%M:%S')
+                'date': date,
+                'updated-at': updated_at
             })
         if not sub:
             continue
