2083a3a23629d66ccdb08d77b1924b186378018a
Baliram Singh <heyome9a@gmail.com>
2023-09-07 11:36:18 +0530

    Update update_docs_map.py
    Signed-off-by: Baliram Singh <heyome9a@gmail.com>

--- blogs/update_docs_map.py
+++ blogs/update_docs_map.py
@@ -4,32 +4,29 @@ from datetime import datetime
 import subprocess
 
 def update_docs_map(docs_dir, docs_map_file):
-    docs_map = []
+    with open(docs_map_file, 'r') as f:
+        docs_map = json.load(f)
+    docs_map_dict = {item['main']: item for item in docs_map}
     for root, dirs, files in os.walk(docs_dir):
         if not files:
             continue
         main = os.path.relpath(root, docs_dir)
-        sub = []
+        if main not in docs_map_dict:
+            docs_map_dict[main] = {'main': main, 'sub': []}
+        sub_dict = {item['name']: item for item in docs_map_dict[main]['sub']}
         for file in files:
             if not file.endswith('.mdx'):
                 continue
             name = os.path.splitext(file)[0]
+            if name not in sub_dict:
+                sub_dict[name] = {'name': name}
             path = os.path.join(root, file)
-            date_str = subprocess.check_output(['git', 'log', '--diff-filter=A', '--format=%aI', '-1', '--', path]).decode().strip()
-            updated_at_str = subprocess.check_output(['git', 'log', '--diff-filter=M', '--format=%aI', '-1', '--', path]).decode().strip()
-            date = datetime.fromisoformat(date_str).strftime('%Y-%m-%d %H:%M:%S') if date_str else 'N/A'
-            updated_at = datetime.fromisoformat(updated_at_str).strftime('%Y-%m-%d %H:%M:%S') if updated_at_str else 'N/A'
-            sub.append({
-                'name': name,
-                'date': date,
-                'updated-at': updated_at
-            })
-        if not sub:
-            continue
-        docs_map.append({
-            'main': main,
-            'sub': sub
-        })
+            date = subprocess.check_output(['git', 'log', '--diff-filter=A', '--format=%aI', '-1', '--', path]).decode().strip()
+            updated_at = subprocess.check_output(['git', 'log', '--diff-filter=M', '--format=%aI', '-1', '--', path]).decode().strip()
+            sub_dict[name]['date'] = datetime.fromisoformat(date).strftime('%Y-%m-%d %H:%M:%S')
+            sub_dict[name]['updated-at'] = datetime.fromisoformat(updated_at).strftime('%Y-%m-%d %H:%M:%S')
+        docs_map_dict[main]['sub'] = list(sub_dict.values())
+    docs_map = list(docs_map_dict.values())
     with open(docs_map_file, 'w') as f:
         json.dump(docs_map, f, indent=4)
 
